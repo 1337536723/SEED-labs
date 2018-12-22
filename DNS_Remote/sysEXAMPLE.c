38f6b95c8f91ea306df01eb5bd6aa00c149be01a
Alexey <33495117+Avigdor-Kolonimus@users.noreply.github.com>
2018-12-23 00:35:33 +0200

    upload files

---
+++ DNS_Remote/sysEXAMPLE.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/ip.h>
+#include <netinet/udp.h>
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <libnet.h>
+
+int main(){
+	system("dig xyz.example.net");
+	return 1;
+}
